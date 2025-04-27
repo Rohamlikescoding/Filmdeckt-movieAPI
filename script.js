e87f7edddc3d37197de49026e2d664313c68d806
Rohamlikescoding <rasadi1704@gmail.com>
2025-04-27 23:05:27 +0330

    it worked
    found out the first API had its own posters, not 4k but will perfectly work thou

--- script.js
+++ script.js
@@ -14,20 +14,33 @@ const findMovie =async function (){
     return json;
     
 }
-const findPoster =async function (){
-    let baseUrl="http://img.omdbapi.com/?apikey=3bc0d31c&t=";
-    // http://www.omdbapi.com/?apikey=3bc0d31c&t=La+la+land&y=2016
-    let n = document.querySelector("#name").value;
-    let year = document.querySelector("#year").value;
-    const movieName = n.replace(/ /g, "+");
-    const fullUrl = `${baseUrl}${movieName}&y=${year}`;
-    console.log(fullUrl);
+// const findPoster =async function (){
+//     let baseUrl="http://img.omdbapi.com/?apikey=3bc0d31c&t=";
+//     // http://www.omdbapi.com/?apikey=3bc0d31c&t=La+la+land&y=2016
+//     let n = document.querySelector("#name").value;
+//     let year = document.querySelector("#year").value;
+//     const movieName = n.replace(/ /g, "+");
+//     const fullUrl = `${baseUrl}${movieName}&y=${year}`;
+//     console.log(fullUrl);
     
-    const response=await fetch(fullUrl);
-    const json=await response.json();
-    return json;
+//     const response=await fetch(fullUrl);
+//     const json=await response.json();
+//     return json;
     
-}
+// }
+// const showDisplay = async function(dataIS) {
+//     if(dataIS){ data => {
+//         document.querySelector('#movie').style.display = "flex";
+//         document.querySelector('#movieName').innerText+= `${data.Title}`;
+//         document.querySelector('#releaseDate').innerText+=`${data.year}`;
+//         document.querySelector('#movie').innerHTML+=`<img src="${data.poster}" alt="${movie.name}"></img>`;
+//         }
+//     }else{
+//         console.log("not working");
+//     }
+    
+// }
+
 const searchHandler=async ()=>{
     // if(!movieName){
     //     alert("please enter movieName!")
@@ -36,11 +49,20 @@ const searchHandler=async ()=>{
     //     alert("please enter year!")
     // }
     const currentData=await findMovie(name,year);
-    console.log(currentData)
-
-    const currentData2=await findPoster(name,year);
-    console.log(currentData)
-    
+    console.log(currentData);
+    if(currentData!=undefined){
+        console.log("working");
+        document.querySelector('#movie').style.display = "flex";
+        // document.querySelector('#movie').style.flexDirection = "column";
+        document.querySelector('#movieName').innerText= `${currentData.Title}`;
+        document.querySelector('#releaseDate').innerText=`${currentData.Year}`;
+        // document.querySelector('#movie').innerHTML+=`<img src="${currentData.Poster}" alt="${movie.name}"></img>`;
+        document.querySelector('#movie').querySelector('img').src =`${currentData.Poster}`;
+    }else{
+        console.log("not working")
+    };
+    return currentData
 }
-
 document.querySelector("#search").addEventListener('click', searchHandler)
+
+
