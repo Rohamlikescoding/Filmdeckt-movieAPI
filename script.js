895da33a44c47c2bc1d71c0502c0d3964dd7bfe4
Rohamlikescoding <rasadi1704@gmail.com>
2025-05-04 12:24:05 +0330

    not perfect but totally works!

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 const findMovie =async function (){
     let baseUrl="http://www.omdbapi.com/?apikey=3bc0d31c&t=";
     // http://www.omdbapi.com/?apikey=3bc0d31c&t=La+la+land&y=2016
@@ -14,32 +13,6 @@ const findMovie =async function (){
     return json;
     
 }
-// const findPoster =async function (){
-//     let baseUrl="http://img.omdbapi.com/?apikey=3bc0d31c&t=";
-//     // http://www.omdbapi.com/?apikey=3bc0d31c&t=La+la+land&y=2016
-//     let n = document.querySelector("#name").value;
-//     let year = document.querySelector("#year").value;
-//     const movieName = n.replace(/ /g, "+");
-//     const fullUrl = `${baseUrl}${movieName}&y=${year}`;
-//     console.log(fullUrl);
-    
-//     const response=await fetch(fullUrl);
-//     const json=await response.json();
-//     return json;
-    
-// }
-// const showDisplay = async function(dataIS) {
-//     if(dataIS){ data => {
-//         document.querySelector('#movie').style.display = "flex";
-//         document.querySelector('#movieName').innerText+= `${data.Title}`;
-//         document.querySelector('#releaseDate').innerText+=`${data.year}`;
-//         document.querySelector('#movie').innerHTML+=`<img src="${data.poster}" alt="${movie.name}"></img>`;
-//         }
-//     }else{
-//         console.log("not working");
-//     }
-    
-// }
 class rate{
     constructor(source, value){
         this.source = source;
@@ -48,55 +21,60 @@ class rate{
     async makeRate(){   
         const s = await this.source;
         const v = await this.value;
-        let answerBack= `<p> ${s}=${v}</p> `;
-        console.log(answerBack);
-        return  answerBack;
+        const answerBack= `<h3> ${s}=${v}</h3> `;
+        // console.log(answerBack);
+        return answerBack
     }
 }
-// let dataNotSet = [];
 
 const rateSource=async (dataNotSet)=>{
-    
-
     let dataSet =[... new Set(dataNotSet)];
-    dataSet.map(async (data) => {
-        console.log(await data);
-        console.log(await data.Source);
-        const newRate = new rate(await data.Source,await data.Value);
-        return await newRate.makeRate()
-    })
+
+    const rateSnippets = await Promise.all(
+        dataSet.map(async (data) => {
+            const newRate = new rate(data.Source, data.Value);
+            return await newRate.makeRate();
+        })
+    );
+    const Rate = rateSnippets.join('');
+    return Rate;
+};
+
+const emptyEntered = ()=>{
+    document.querySelector("#name").value="";
+    document.querySelector("#year").value="";
 }
 
-const searchHandler=async ()=>{
-    // if(!movieName){
-    //     alert("please enter movieName!")
-    // }
-    // if(!year){
-    //     alert("please enter year!")
-    // }
-    
-    const currentData=await findMovie(name,year);
-    console.log(currentData);
-    if(currentData!=undefined){
-        console.log("working");
-        document.querySelector('#movie').style.display = "flex";
-        // document.querySelector('#movie').style.flexDirection = "column";
-        document.querySelector('#movieName').innerText= `${currentData.Title}`;
-        document.querySelector('#releaseDate').innerText=`${currentData.Year}`;
-        // document.querySelector('#movie').innerHTML+=`<img src="${currentData.Poster}" alt="${movie.name}"></img>`;
-        document.querySelector('#rate').innerText =`${currentData.Rated}`;
-        document.querySelector('#movie').querySelector('img').src =`${currentData.Poster}`;
-        document.querySelector('#actors').innerText= `${currentData.Actors}`;
-        document.querySelector('#awards').innerText=`${currentData.Awards}`;
-        document.querySelector('#country').innerText= `${currentData.Country}`;
-        document.querySelector('#plot').innerText= `${currentData.Plot}`;
-        const Ratings = currentData.Ratings;
-        document.querySelector('#ratings').innerHTML=await rateSource(Ratings);
 
+const searchHandler=async ()=>{
+    if(!document.querySelector("#name").value){
+        alert("please enter movieName!")
+    }
+    if(!document.querySelector("#year").value){
+        alert("please enter year!")
     }else{
-        console.log("not working")
-    };
-    return currentData
+        const currentData=await findMovie(name,year);
+        console.log(currentData);
+        if(currentData!=undefined){
+            console.log("working");
+            document.querySelector('#movie').style.display = "flex";
+            document.querySelector('#movieName').innerText= `${currentData.Title}`;
+            document.querySelector('#releaseDate').innerText=`${currentData.Year}`;
+            document.querySelector('#rate').innerText =`${currentData.Rated}`;
+            document.querySelector('#movie').querySelector('img').src =`${currentData.Poster}`;
+            document.querySelector('#actors').innerText= `${currentData.Actors}`;
+            document.querySelector('#awards').innerText=`${currentData.Awards}`;
+            document.querySelector('#country').innerText= `${currentData.Country}`;
+            document.querySelector('#plot').innerText= `${currentData.Plot}`;
+            const Ratings = currentData.Ratings;
+            document.querySelector('#ratings').innerHTML= await rateSource(Ratings);
+            emptyEntered()
+        }else{
+            console.log("not working")
+        };
+        
+        return currentData
+    }
 }
 document.querySelector("#search").addEventListener('click', searchHandler)
 
